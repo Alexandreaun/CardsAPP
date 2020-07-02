0b2627b599d6ff0dfb87f5f3b0763b35b2929d01
alexandreaun <alexandreaun@brq.com.br>
2020-07-02 09:22:14 -0300

    projeto iniciado, configuracao do target definida

---
+++ CardsProjectAPP/CardsProjectAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardsProjectAPP
+//
+//  Created by Alexandre Carlos Aun on 02/07/20.
+//  Copyright © 2020 Alexandre Carlos Aun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
